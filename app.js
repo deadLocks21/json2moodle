6e8487cbb6bd4526b4c3d23987991addce1885b7
Timothé HOFMANN <timothe@logomotion.fr>
2021-06-29 14:58:22 +0200

    Gestion de la connexion et de l'accès à la formation.

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
-// const identifiers = require('./identifiers').identifiers;
-const puppeteer = require('puppeteer');
-const {login, password, main_address} = require('./variables');
+const puppeteer = require("puppeteer");
+let { login, password, main_address, course_name } = require("./variables");
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'screens/example.png' });
+
+  // Get the course
+  await page.goto(main_address);
+  let links = await page.$$eval("h3.coursename a", (els) =>
+    els.map((el) => {
+      return { course_name: el.innerHTML, link: el.getAttribute("href") };
+    })
+  );
+
+  let course_page_link;
+  for (let i = 0; i < links.length; ++i) {
+    if (links[i].course_name == course_name) course_page_link = links[i].link;
+  }
+
+  if (course_page_link === undefined) {
+    await browser.close();
+    console.log(`The course ${course_name} doesn't exists ...`);
+    return;
+  }
+
+
+
+
+  // Go to login page
+  await page.goto(`${main_address}/login/index.php`);
+
+  await page.click("#username");
+  await page.keyboard.type(login);
+
+  await page.click("#password");
+  await page.keyboard.type(password);
+
+  await page.click("#loginbtn");
+  await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+
+
+  // Screen end page
+  await page.screenshot({ path: "screens/example.png" });
 
   await browser.close();
-
-  console.log(login);
 })();
